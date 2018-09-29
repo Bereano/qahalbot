43a9d2b96a5dc724257b7ed2276a0ad4176b8f31
Bereano <manuel.galiotto@hotmail.com>
2018-09-29 18:52:09 +0200

    Revert "."
    This reverts commit 9fb95d6c4f3fcf069fcfab412fa93f98f4a6074c.

--- index.php
+++ index.php
@@ -1,7 +1,1 @@
-<?php //echo "Silence is golden";
-
-try {
-  throw new Exception("Test Eccezione!");
-} catch (Exception $e) {
-  echo 'Caught exception: ' . $e->getMessage();
-}
+<?php echo "Silence is golden";
