9fb95d6c4f3fcf069fcfab412fa93f98f4a6074c
Bereano <manuel.galiotto@hotmail.com>
2018-09-29 18:51:15 +0200

    .

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-<?php echo "Silence is golden";
+<?php //echo "Silence is golden";
+
+try {
+  throw new Exception("Test Eccezione!");
+} catch (Exception $e) {
+  echo 'Caught exception: ' . $e->getMessage();
+}
